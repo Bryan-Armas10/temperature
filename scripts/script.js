47617033edead77be1e7ddd9e9c11cc4259a15af
Bryan-Armas10 <bryan.armas@sdgku.edu>
2024-09-28 12:19:49 -0700

    Class 1

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+console.log("Scipt");
+
+// Creating string variables
+// Keyword nameVariable = value;
+let myName = "Jane";
+let lastName = "Doe";
+
+console.log(myName);
+console.log(lastName);
+console.log(myName + " " +lastName);//Concatenation
+
+// Creating numerical variables
+let myAge = 99;
+console.log("Age: " + myAge);
+
+let myGrade = 9.5;
+console.log(myGrade);
+
+// Creating a Boolean vvariables  (true/false)
+let isAdmin = true;
+let isSudent = false;
+let isProfesor = true;
+console.log(isProfesor);
+
+/* mani lines of coments
+xxxx
+xxxx
+xxxx
+xxxx
+*/
+
+myName = "Samantha";
+lastName = "Jimenez";
+// my name is Samantha Jimenez, and I am 99 years old.
+console.log("My name is: " + myName + " " + lastName + ", and I am " + myAge + " " + "years old" );
+
+let number1 = 10;
+let number2 = 5;
+
+// addition
+let addition = number1 + number2;
+console.log(number1 + " + " + number2 + " = " + addition);
+// substraction
+let substraction = number1 - number2;
+console.log(number1 + " - " + number2 + " = " + substraction);
+// Multiplication
+let multiplication = number1 * number2;
+console.log(number1 + " * " + number2 + " = " + multiplication);
+// Divicions
+let divicions = number1 / number2;
+console.log(number1 + " / " + number2 + " = " + divicions);
+
+// Constants
+// keyword constName = value;
+
+const pi = 3.1416;
+// pi = 4.5 // we can't modify a constant
+console.log(pi);
