a7448dfcf03e4a22052b08e74aa5e0d88aff08b4
Bryan-Armas10 <bryan.armas@sdgku.edu>
2024-10-04 15:53:39 -0700

    Assignment 3

--- scripts/temperature2.js
+++ scripts/temperature2.js
@@ -1,5 +1,16 @@
-function temperature(){
-    let celsius = prompt("Enter temperature (celsius): ");
-    fahrenheit = (celsius * 9/5) + 32;
-    document.getElementById("result").innerHTML = `<p> Fahrenheit: ${fahrenheit} </p>`;
+function convertTemperature() {
+    let scale = prompt("Do you want to convert to Celsius (C) or Fahrenheit (F)?").toUpperCase();
+    let temperature;
+
+    if (scale === "C") {
+        temperature = prompt("Enter temperature Celsius: ");
+        let fahrenheit = (temperature * 9 / 5) + 32;
+        document.getElementById("result").innerHTML = `<p>Fahrenheit: ${fahrenheit.toFixed(2)}°</p>`;
+    } else if (scale === "F") {
+        temperature = prompt("Enter temperature Fahrenheit: ");
+        let celsius = (temperature - 32) * 5 / 9;
+        document.getElementById("result").innerHTML = `<p>Celsius: ${celsius.toFixed(2)}°</p>`;
+    } else {
+        document.getElementById("result").innerHTML = `<p>Please use "C" or "F"</p>`;
+    }
 }
